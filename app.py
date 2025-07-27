3d2a327de59bdcad4541a76a3d7683a519103a61
Manan Meemroth <divyanshmeemroth@gmail.com>
2025-07-28 02:31:41 +0530

    Specify Python version for Streamlit Cloud

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model = load_model('/Users/mananmeemroth/Projects/Stock predictor/Stock Predictions Model.keras')
+model = load_model("Stock Predictions Model.keras")
 
 st.header('Stock Market Predictor')
 
